5480cd38a993c7adf9ea2284e73d4fcdd21fddb7
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2021-03-13 19:51:23 +0100

    1st fully functional based on specification
    let's say we could use this one as version 1.0.0

--- wlc-add-guest.py
+++ wlc-add-guest.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2
 # 20210110, Ing. Ondrej DURAS (dury), Orange Slovensko a.s. ,GPL2
-#=vim high Comment ctermfg=brown
+#=vim color desert
+#=vim high pythonBuiltin ctermfg=darkcyan
+#=vim high Comment ctermfg=darkgreen
+#=vim syntax match Comment /^\s*#.*$/
 
 ## MANUAL ############################################################# {{{ 1
 
-VERSION = "2021.031201"
+VERSION = "2021.031301"
 MANUAL  = """
 NAME: Add Guest User on WLC
 FILE: wlc-add-guest.py
@@ -76,6 +79,9 @@ VERSION: %s
 ####################################################################### }}} 1
 ## GLOBAL modules and variables ####################################### {{{ 1
 
+# List of modules used in this script
+# all modules are native - included in installation file python-2.7.17.msi
+# ... so none needs to be added via pip utility. 
 import sys
 import os
 import re
@@ -89,8 +95,15 @@ import hashlib
 import base64
 
 
-CONFIG={}  # script CONFIGuration structure
-AUTHCODE_INTERNAL = 536034627567673L 
+CONFIG = {}  # script CONFIGuration structure / stored .JSON in configuration file
+AUTHCODE_INTERNAL = 536034627567673L # authorization code of the script instance
+
+# CLI parameters, collected by cliParameters() handled by takeAction()
+VUSER  = ""  # visitor's username given trough CLI
+VPASW  = ""  # visitor's password -//-
+VDESC  = ""  # visitor's description
+ACTION = ""  # action / activity, taken based on collection of CLI parameters
+
 
 ####################################################################### }}} 1
 ## CONFIG handling #################################################### {{{ 1
@@ -121,9 +134,46 @@ def configLoad(filename=configFileName()):
   
 
 ####################################################################### }}} 1
+## WLC SSH Sessions ################################################### {{{ 1
+
+def wlcExec(host,user,pasw,action,save="N"):
+  # Opening and authenticating the SSH session with the WLC
+  command="plink.exe -no-antispoof -batch -ssh -l %s %s" % (user,host)
+  stdin,stdout = os.popen4(command)
+  stdin.write(user + "\r") # login is provided twice ... :-)
+  stdin.write(pasw + "\r") # password (authentication method=none, then chat
+  stdin.write("config paging disable\r") # disabling --more--
+
+  # Pushing activity
+  #stdin.write("show sysinfo\r")   # AirOS version
+  #stdin.write("show inventory\r") # Harware inventory
+  #stdin.write("show wlan summary" + "\r")     # or anything else
+  for line in action.splitlines():
+    stdin.write(line + "\r")
+
+  # Logout and session termination
+  stdin.write("logout\r")   # command to exit
+  stdin.write(save + "\n")  # do not write configuration
+  stdin.close()             # that point realy starts the session
+  data = ""                 # read everything in single string
+  for line in stdout.readlines():
+    if re.match("User:",line):     continue # removes confidetial parts of the session
+    if re.match("Password:",line): continue
+    data += line.rstrip() + "\n"
+  stdout.close()            # closing SSH session
+  return data               # returns output of the session
+
+####################################################################### }}} 1
 ## AUTHORIZATION ###################################################### {{{ 1
 
+# everything related to authorization of this script
+# includes also AutoLogin and AutoPassword calculations used
+# byt script to access the controller.
 
+
+# Provides an uniqueID of the script
+# if script changes (even a comment within the script), then
+# the script's ID changes too ...and then it requeres re-authorization
 def pwaScriptID(filename=__file__):
   sf=open(filename,"r")
   sfdata=sf.read()
@@ -132,8 +182,9 @@ def pwaScriptID(filename=__file__):
   sum.update(sfdata)
   return base64.b32encode(sum.digest())[3:13]
 
-
-# fqdn=socket.getfqdn(socket.gethostbyname(socket.gethostname()))
+# All details that should be enought to authorize this script.
+# They are intented to be used for remote authorization within later versions.
+# # fqdn=socket.getfqdn(socket.gethostbyname(socket.gethostname()))
 def pwaAutoIdentity():
   authbase = pwaScriptID() + "\\"
   try: 
@@ -146,25 +197,31 @@ def pwaAutoIdentity():
   authbase += str(getmac())
   return authbase
 
+# describes a source of authomated user HOSTNAME-LOGIN where from
+# an activities have come
+def pwaAutoDescription():
+    return socket.gethostname() + "-" + getpass.getuser()
 
+# Authorized Login name, used to access the controller
 def pwaAutoLogin(identity=pwaAutoIdentity()):  # pwaRobot()
   return "AUTO" + base64.b32encode(hashlib.sha1(identity).digest())[3:10]
 
+# Authorized Password, used to access the controller
 def pwaAutoPassword(identity=pwaAutoIdentity()):  # pwaToken()
   global CONFIG,AUTHCODE_INTERNAL
   paswrc = str(AUTHCODE_INTERNAL) + str(CONFIG["wlan"])
   paswrc += identity + str(CONFIG["exkey"])
   return base64.b64encode(hashlib.sha1(paswrc).digest())[3:23]
 
-
+# Provides whole action of the Script Authorization
 def wlcAuthorize():
-  global CONFIG
+  global CRED,CONFIG
   random.seed()
   # collecting session details
   host  = raw_input(      "WLC IP address or Hostname ..... : ")
-  wlan  = raw_input(      "WLC WLAN ID (number) ........... : ")
   auser = raw_input(      "WLC Admin Login ................ : ")
   apasw = getpass.getpass("WLC Admin Password ............. : ")
+  wlan  = raw_input(      "WLC WLAN ID (number) ........... : ")
   exkey = random.randrange(10000000000L,99999999999L)
 
   # Saving configuration
@@ -172,48 +229,142 @@ def wlcAuthorize():
   CONFIG["wlan"]  = wlan
   CONFIG["host"]  = host
   configSave()
-  
+  print "Configuration saved."
+ 
   # Preparing technicalities for SSH session 
   xuser = pwaAutoLogin()
   xpasw = pwaAutoPassword()
-  command="plink.exe -no-antispoof -batch -ssh -l %s %s" % (auser,host)
-
-  #> Tu pokracovat upravou SSH relacie
-  # Opening and authenticating the SSH session with the WLC
-  stdin,stdout = os.popen4(command)
-  stdin.write(auser + "\r") # login is provided twice ... :-)
-  stdin.write(apasw + "\r") # password (authentication method=none, then chat
-  stdin.write("config paging disable\r") # disabling --more--
-
-  # Pushing activity
-  stdin.write("show sysinfo\r")   # AirOS version
-  stdin.write("show inventory\r") # Harware inventory
-  stdin.write("show wlan summary" + "\r")     # or anything else
-
-  # Logout and session termination
-  stdin.write("logout\r")   # command to exit
-  stdin.write("N\n")        # do not write configuration
-  stdin.close()             # that point realy starts the session
-  data=stdout.read()        # read everything in single string
-  print data #DEBUG
-  stdout.close()            # closing SSH session
-  return True
+  xdesc = pwaAutoDescription() 
+  # config mgmtuser add AUTO12341234 Hello.Hello+ read-write ScriptID12341234-Automat
+  # show mgmtuser
+  # config mgmtuser delete AUTO12341234
+  action = "config mgmtuser add %s %s read-write %s\n" % (xuser,xpasw,xdesc)
+  print "Connecting WLC ..."
+  wlcExec(host,auser,apasw,action,"y")
+  print "Stript registered as %s" % (xuser)
+  print "Checking Authentication..."
+  data = wlcExec(host,xuser,xpasw,"show mgmtuser\n")
+  if xuser in data:
+    print "Script has been authorized successfully."
+    return True
+  else:
+    print "WARNING: Something wrong has happened."
+    return False
 
 ####################################################################### }}} 1
 ## GUEST handling ##################################################### {{{ 1
 
-####################################################################### }}} 1
-## MAIN ############################################################### {{{ 1
+# procedures handling all visitors' WiFi accesses
 
-if __name__ == "__main__":
+# Adds new visitor's account to access corporate WiFi
+def wlcAddGuest(vuser,vpasw,vdesc=""):
+  configLoad()
+  host = CONFIG["host"]
+  wlan = CONFIG["wlan"]
+  xuser= pwaAutoLogin()
+  xpasw= pwaAutoPassword()
+  if not vdesc:
+    vdesc = "by" + xuser
+  if not (host and xuser and xpasw and wlan):
+     print "Error: Script Configuration Issue !"
+     return False
+  action  = "config netuser add %s %s wlan %s " % (vuser,vpasw,wlan)
+  action += "userType guest lifetime 86400 description %s\n" % (vdesc)
+  action += "show netuser summary\n"
+  data = wlcExec(host,xuser,xpasw,action)
+  return True
+
+
+# Lists still valid viritors' accesses
+def wlcListGuests():
+  configLoad()
+  host = CONFIG["host"]
+  xuser= pwaAutoLogin()
+  xpasw= pwaAutoPassword()
+  if not (host and xuser and xpasw):
+     print "Error: Script Configuration Issue !"
+     return False
+  action  = "show netuser summary\n";
+  print wlcExec(host,xuser,xpasw,action)
+  return True
+
+
+####################################################################### }}} 1
+## ACTIONS ############################################################ {{{ 1
+
+# checks parameters given by command line / semantic check
+# then calls appropriate procedure to take an action
+
+def takeAction():
+  global VUSER,VPASW,VDESC,ACTION
+
+  # adds visitor's access to WiFi
+  if ACTION == "addGuest":
+     if not (VUSER and VPASW):
+        print "Error: addGuest: user (-u) and password (-p) must be provided !"
+     wlcAddGuest(VUSER,VPASW,VDESC)
+     exit()
+
+  # authorizes this script. Makes its own admin account with 
+  # the calculated login and password.
+  # Access password is not stored anywhere.
+  if ACTION == "authorizeScript":
+     wlcAuthorize()
+     exit()
+
+  # provides an list of valid visitors' accounts
+  if ACTION == "listGuests":
+     wlcListGuests()
+     exit()
+
+####################################################################### }}} 1
+## CLI interface - handling CLI parameters ############################ {{{ 1
+
+# collects command-line parameters
+# and does their syntax check
+# also it assumes an ACTION which would be taken
+
+def cliParameters():
+  global VUSER,VPASW,VDESC,ACTION
   argct = len(sys.argv)
   if argct < 2: print MANUAL; exit()
   argix = 1
   while(argix < argct):
     argx = sys.argv[argix]; argix += 1
+
     if argx in ("-a","--authorize"):
-       wlcAuthorize()
-      
+       ACTION = "authorizeScript"
+       continue
+
+    if argx in ("-l","--list"):
+       ACTION = "listGuests"
+       continue
+
+    if argx in ("-u","--user"):
+       VUSER = sys.argv[argix]; argix += 1
+       ACTION = "addGuest" 
+       continue
+
+    if argx in ("-p","--password"):
+       VPASW = sys.argv[argix]; argix += 1
+       ACTION = "addGuest" 
+       continue
+
+    if argx in ("-d","--description"):
+       VDESC = sys.argv[argix]; argix += 1
+       ACTION = "addGuest" 
+       continue
+
+
+####################################################################### }}} 1
+## MAIN ############################################################### {{{ 1
+
+# the Main part of the programm
+# it should be as short as possible
+if __name__ == "__main__":
+  cliParameters()
+  takeAction()
+    
 ####################################################################### }}} 1
 # --- end ---
 
